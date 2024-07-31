a1bac931c703ceb512459289552baf921e8ce036
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-07-31 13:08:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+// src/app.js
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-//Using app.listen create your first express server on port 3000
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
 module.exports = app;
