bbc910132b70bae760d50fa765ebe16a13a061ea
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-07-31 13:20:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-// src/app.js
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Internal Server Error:', err);
+  res.status(500).send('Internal Server Error');
 });
 
 module.exports = app;
