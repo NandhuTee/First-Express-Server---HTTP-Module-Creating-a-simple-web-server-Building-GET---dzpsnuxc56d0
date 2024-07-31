c56893b1a59f73b72d2de09757a80b87bcb7a518
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-07-31 13:18:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-// src/index.js
 const dotenv = require('dotenv');
 const app = require('./app');
-const mongoose = require('mongoose');
 
-// Load environment variables from .env file
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+try {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+} catch (error) {
+  console.error('Error starting server:', error);
+}
